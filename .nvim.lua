a67a82040cb8724533e790a29dc6a81dee1e1ba5
Fabiano França <fabiano.franca@gmail.com>
2026-08-25 15:50:41 +0100

    Enable clojure-lsp

--- .nvim.lua
+++ .nvim.lua
@@ -1,3 +1,14 @@
+-- disable K mapping
+vim.g["conjure#mapping#doc_word"] = false
+
+vim.lsp.config('clojure-lsp', {
+  cmd = { 'clojure-lsp' },
+  filetypes = { 'clojure', 'edn' },
+  root_markers = { 'project.clj', 'deps.edn', 'build.boot', 'shadow-cljs.edn', '.git', 'bb.edn' },
+})
+
+vim.lsp.enable('clojure-lsp')
+
 local MiniDeps = require('mini.deps')
 
 MiniDeps.setup()
